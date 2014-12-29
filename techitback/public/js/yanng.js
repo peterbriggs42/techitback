6019729e83599cd4c63a990a7c437121ab64ae2d
Will Tachau <wtachau@gmail.com>
2014-12-29 11:09:39 -0800

    including header as html server side render

--- techitback/public/js/yanng.js
+++ techitback/public/js/yanng.js
@@ -8,49 +8,28 @@ jQuery(function($) {
 	 * javascript. The latter will check for the specified element.
 	 */
 
-	function yanng_listener (element, prescript, header_text, URL) {
+	function yanng_listener (element, URL) {
 		$(document).on('click', element, function () {
-			if (prescript != null) {
-				prescript();
-			}
-		 	// change header
-		 	if (header_text != null) {
-				$("#yanng_header h2").html(header_text);
-			}
 			// change body
 			ajaxRequest(URL, '', 'GET', function(data) {
-				$(".yanng_body").html(data)
+				$(".yanng_content").html(data)
 			});
 		});
 	}
 
-	function show_header(tips_shown) {
-		if (tips_shown) {
-			$("#yanng_header").hide();
-			$("#yanng_tips_header").show();
-		} else {
-			$("#yanng_header").show();
-			$("#yanng_tips_header").hide();
-		}
-	}
-
 	// Back to YANNG home
-	yanng_listener(".yanng_header_button", function() {
-			show_header(false);
-		}, "Exclusive Tips from the Yes and No No Girls", 'ajax/yanng_home');
+	yanng_listener(".yanng_header_button", 'ajax/yanng_home');
 	
 	// Clicking "About"
-	yanng_listener(".about_link", null, "About Yes and No No Girls", 'ajax/yanng_about');
+	yanng_listener(".about_link", 'ajax/yanng_about');
 	
 	// Clicking second "About" page
-	yanng_listener(".yanng_body .next_page", null, null, 'ajax/yanng_about2');
+	yanng_listener(".yanng_body .next_page", 'ajax/yanng_about2');
 	
 	// Clicking "Tech Etiquette"
-	yanng_listener("#etiquette", null, "Yes and No No Girls (AKA Yanng): Tech Etiquette", 'ajax/yanng_etiquette');
+	yanng_listener("#etiquette", 'ajax/yanng_etiquette');
 
 	// Tips
-	yanng_listener(".tip_link", function() { 
-			show_header(true);
-		}, null, 'ajax/yanng_tips');
+	yanng_listener(".tip_link", 'ajax/yanng_tips');
 
 });
