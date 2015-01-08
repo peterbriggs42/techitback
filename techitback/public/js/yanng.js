66aa80c357d9a4746a0d0250cfd12e4721204bbf
Will Tachau <wtachau@gmail.com>
2015-01-08 14:26:19 -0800

    loading gif

--- techitback/public/js/yanng.js
+++ techitback/public/js/yanng.js
@@ -8,53 +8,62 @@ jQuery(function($) {
 	 * javascript. The latter will check for the specified element.
 	 */
 
-	function yanng_listener (element, URL, replace) {
+	function yanng_listener (element, URL, replace, fade) {
 		$(document).on('click', element, function (e) {
 			e.stopPropagation();
+
+			var replaceContent = ".yanng_content";
+			if (replace!= null) {
+				replaceContent = replace;
+			}
+
+			$(replaceContent).html("<div id='loading_body'><img src='images/loading.gif'></div>");
+
 			// change body
 			ajaxRequest(e, URL, '', 'GET', function(data) {
-				var replaceContent = ".yanng_content";
-				if (replace!= null) {
-					replaceContent = replace;
-				}
-				var timing = 50;
-			    $( replaceContent ).fadeOut( timing , function(){
+				
+				if (fade) {
+					var timing = 50;
+				    $( replaceContent ).fadeOut( timing , function(){
+				    	$(replaceContent).html(data);
+				    	$( replaceContent ).fadeIn( timing );
+					});
+			    } else {
 			    	$(replaceContent).html(data);
-			    	$( replaceContent ).fadeIn( timing );
-				});
+			    }
 			});
 		});
 	}
 
 	// Add a listener event for element and corresponding ajax request
 	[ 	// Main links	
-		{'element':".yanng_header_button",	'url':'ajax/yanng_home'			, 'replace':null},
-		{'element':".about_link", 			'url':'ajax/yanng_about'		, 'replace':null},
-		{'element':".yanng_body .next_page",'url':'ajax/yanng_about2'		, 'replace':null},
-		{'element':"#etiquette", 			'url':'ajax/yanng_etiquette'	, 'replace':null},
-		{'element':"#meetus", 				'url':'ajax/yanng_meetus'		, 'replace':null},
-		{'element':".tips_link", 			'url':'ajax/yanng_tips'			, 'replace':null},
-		{'element':"#create", 				'url':'ajax/yanng_createshare'	, 'replace':null},
+		{'element':".yanng_header_button",	'url':'ajax/yanng_home'			, 'replace':null, 'fade':true},
+		{'element':".about_link", 			'url':'ajax/yanng_about'		, 'replace':null, 'fade':true},
+		{'element':".yanng_body .next_page",'url':'ajax/yanng_about2'		, 'replace':null, 'fade':true},
+		{'element':"#etiquette", 			'url':'ajax/yanng_etiquette'	, 'replace':null, 'fade':true},
+		{'element':"#meetus", 				'url':'ajax/yanng_meetus'		, 'replace':null, 'fade':true},
+		{'element':".tips_link", 			'url':'ajax/yanng_tips'			, 'replace':null, 'fade':true},
+		{'element':"#create", 				'url':'ajax/yanng_createshare'	, 'replace':null, 'fade':true},
 		// Tip sections
-		{'element':".tips_footer", 			'url':'ajax/yanng_etiquette'	, 'replace':null},
-		{'element':".tip1_link", 			'url':'ajax/yanng_tips/tips1'	, 'replace':null},
-		{'element':".tip2_link", 			'url':'ajax/yanng_tips/tips2'	, 'replace':null},
-		{'element':".tip3_link", 			'url':'ajax/yanng_tips/tips3'	, 'replace':null},
-		{'element':".tip4_link", 			'url':'ajax/yanng_tips/tips4'	, 'replace':null},
-		{'element':".tip5_link", 			'url':'ajax/yanng_tips/tips5'	, 'replace':null},
-		{'element':".tip6_link", 			'url':'ajax/yanng_tips/tips6'	, 'replace':null},
-		{'element':".tip7_link", 			'url':'ajax/yanng_tips/tips7'	, 'replace':null},
-		{'element':".tip8_link", 			'url':'ajax/yanng_tips/tips8'	, 'replace':null},
-		{'element':".tip9_link", 			'url':'ajax/yanng_tips/tips9'	, 'replace':null},
-		{'element':".tip10_link", 			'url':'ajax/yanng_tips/tips10'	, 'replace':null},
-		{'element':".tip11_link", 			'url':'ajax/yanng_tips/tips11'	, 'replace':null},
-		{'element':".tip12_link", 			'url':'ajax/yanng_tips/tips12'	, 'replace':null},
-		{'element':".tip13_link", 			'url':'ajax/yanng_tips/tips13'	, 'replace':null},
+		{'element':".tips_footer", 			'url':'ajax/yanng_etiquette'	, 'replace':null, 'fade':false},
+		{'element':".tip1_link", 			'url':'ajax/yanng_tips/tips1'	, 'replace':null, 'fade':false},
+		{'element':".tip2_link", 			'url':'ajax/yanng_tips/tips2'	, 'replace':null, 'fade':false},
+		{'element':".tip3_link", 			'url':'ajax/yanng_tips/tips3'	, 'replace':null, 'fade':false},
+		{'element':".tip4_link", 			'url':'ajax/yanng_tips/tips4'	, 'replace':null, 'fade':false},
+		{'element':".tip5_link", 			'url':'ajax/yanng_tips/tips5'	, 'replace':null, 'fade':false},
+		{'element':".tip6_link", 			'url':'ajax/yanng_tips/tips6'	, 'replace':null, 'fade':false},
+		{'element':".tip7_link", 			'url':'ajax/yanng_tips/tips7'	, 'replace':null, 'fade':false},
+		{'element':".tip8_link", 			'url':'ajax/yanng_tips/tips8'	, 'replace':null, 'fade':false},
+		{'element':".tip9_link", 			'url':'ajax/yanng_tips/tips9'	, 'replace':null, 'fade':false},
+		{'element':".tip10_link", 			'url':'ajax/yanng_tips/tips10'	, 'replace':null, 'fade':false},
+		{'element':".tip11_link", 			'url':'ajax/yanng_tips/tips11'	, 'replace':null, 'fade':false},
+		{'element':".tip12_link", 			'url':'ajax/yanng_tips/tips12'	, 'replace':null, 'fade':false},
+		{'element':".tip13_link", 			'url':'ajax/yanng_tips/tips13'	, 'replace':null, 'fade':false},
 		// Meet Us sections
-		{'element':".meetus_body", 				'url':'ajax/yanng_meetus'		, 'replace':null},
-		{'element':".meetus_body #shlee img",	'url':'ajax/yanng_girls/shlee'	, 'replace':".meetus_body .row_container"},
-		{'element':".meetus_body #goldie img",	'url':'ajax/yanng_girls/goldie'	, 'replace':".meetus_body .row_container"},
-		{'element':".meetus_body #yumi img",	'url':'ajax/yanng_girls/yumi'	, 'replace':".meetus_body .row_container"}
+		{'element':".meetus_body", 				'url':'ajax/yanng_meetus'		, 'replace':null, 'fade':true},
+		{'element':".meetus_body #shlee img",	'url':'ajax/yanng_girls/shlee'	, 'replace':".meetus_body .row_container", 'fade':false},
+		{'element':".meetus_body #goldie img",	'url':'ajax/yanng_girls/goldie'	, 'replace':".meetus_body .row_container", 'fade':false},
+		{'element':".meetus_body #yumi img",	'url':'ajax/yanng_girls/yumi'	, 'replace':".meetus_body .row_container", 'fade':false}
 
 		
 	].map( function(listener) {
