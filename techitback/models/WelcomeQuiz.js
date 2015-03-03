3999310420c7ffe7adb9c0c6ceb3658ee016a967
Will Tachau <wtachau@gmail.com>
2015-03-02 20:35:49 -0800

    changed quiz links to be editable

--- techitback/models/WelcomeQuiz.js
+++ techitback/models/WelcomeQuiz.js
@@ -16,7 +16,7 @@ var WelcomeQuizzes = new keystone.List('Welcome Quiz', {
 WelcomeQuizzes.add({
 	title: { type: String, required: true },
 	key: { type:Types.Key, noedit: true},
-	link: {type: String, noedit: true}
+	link: {type: String, noedit: false}
 });
 
 WelcomeQuizzes.defaultColumns = 'title';
