3cf456cada1bec7ab7395156e4f5fb7d0b664010
Will Tachau <wtachau@gmail.com>
2015-03-02 22:42:36 -0800

    making downloadable section

--- techitback/routes/ajax/home/welcome/downloadable.js
+++ techitback/routes/ajax/home/welcome/downloadable.js
@@ -10,14 +10,21 @@ exports = module.exports = function(req, res) {
  
 	WelcomeSections.model.find()
 		.exec(function(err, sections) {
+
+		var Downloadables = keystone.list('Downloadable');
+ 
+		Downloadables.model.find()
+			.exec(function(err, downloadables) {	
 		
-		view.render('home/welcome_downloadable', {
-			main: 			sections.filter(common.getItemByKey, "downloadable")[0],
-			one: 			sections.filter(common.getItemByKey, "video")[0],
-			two: 			sections.filter(common.getItemByKey, "causes")[0],
-			three: 			sections.filter(common.getItemByKey, "quizzes")[0],
-			four: 			sections.filter(common.getItemByKey, "join")[0],
-			five: 			sections.filter(common.getItemByKey, "games")[0]
+			view.render('home/welcome_downloadable', {
+				main: 			sections.filter(common.getItemByKey, "downloadable")[0],
+				one: 			sections.filter(common.getItemByKey, "video")[0],
+				two: 			sections.filter(common.getItemByKey, "causes")[0],
+				three: 			sections.filter(common.getItemByKey, "quizzes")[0],
+				four: 			sections.filter(common.getItemByKey, "join")[0],
+				five: 			sections.filter(common.getItemByKey, "games")[0],
+				downloads: 		downloadables,
+			});
 		});
 	});
 };
